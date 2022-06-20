cf9babcd45dfee2d765b1d459eb106dd01aa3859
vinishd <63834401+vinishd@users.noreply.github.com>
2022-06-20 16:37:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-    "apiKey": "*YOUR OWN API KEY*",
+    "apiKey": "476586d174af6b78b295f81b28486ebb",
     fetchWeather: function(city) {
         fetch(
                 "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -38,7 +38,7 @@ let weather = {
 
 let geocode = {
     reverseGeoCode: function(latitude, longitude) {
-        var api_key = '*YOUR OWN API KEY*';
+        var api_key = '93e9333898a94e389628d08c3faf8c1f';
         var api_url = 'https://api.opencagedata.com/geocode/v1/json'
 
         var request_url = api_url +
